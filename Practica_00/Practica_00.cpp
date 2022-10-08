ceb685688479da0ddcec4b809eac99955fe81974
Roberto A. Vivó Hernando <rvivo@upv.edu.es>
2022-10-08 19:48:11 +0200

    Carga inicial de proyecto ejemplo

---
+++ Practica_00/Practica_00.cpp
@@ -0,0 +1,46 @@
+/***************************************************
+*	Practica_00.cpp
+* 
+*	Codigo ejemplo inicial para practicas con OpenGL
+*	y GLUT en la asignatura ISGI/GIINF/UPV
+*	
+*	@author	R.Vivo' <rvivo@upv.es>
+* 
+***************************************************/
+#define PROYECTO "Primer programa en OpenGL"
+
+#include <iostream>									
+#include <GL\freeglut.h>							
+
+// Funcion de atencion al evento de dibujo
+void display()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutWireTeapot(0.5);
+	glFlush();
+}
+
+// Funcion de atencion al redimensionamiento
+void reshape(GLint w, GLint h)
+{
+}
+
+int main(int argc, char** argv)
+{
+	// Inicializaciones
+	glutInit(&argc, argv);							
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);	
+	glutInitWindowSize(500, 400);					
+	glutInitWindowPosition(50, 50);
+
+	// Crear ventana
+	glutCreateWindow(PROYECTO);		
+
+	// Registrar callbacks
+	glutDisplayFunc(display);						
+	glutReshapeFunc(reshape);	
+
+	// Bucle de atencion a eventos
+	glutMainLoop();									
+}
