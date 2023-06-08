4dab6eb7f84f01c72b6999c56c76e8bb9f5fa258
R.Vivo <rvivo@upv.es>
2023-06-08 15:50:44 +0200

    Refactory for ETSINF

--- Primer_Proyecto/Primer_Proyecto.cpp
+++ Primer_Proyecto/Primer_Proyecto.cpp
@@ -1,11 +1,11 @@
 /***************************************************
-*	Practica_00.cpp
-* 
+*	Primer_Proyecto.cpp
+*
 *	Codigo ejemplo inicial para practicas con OpenGL
-*	y GLUT en la asignatura ISGI/GIINF/UPV
-*	
+*	y GLUT para ETSINF/UPV
+*
 *	@author	R.Vivo' <rvivo@upv.es>
-* 
+*
 ***************************************************/
 #define PROYECTO "Primer programa en OpenGL"
 
@@ -29,18 +29,18 @@ void reshape(GLint w, GLint h)
 int main(int argc, char** argv)
 {
 	// Inicializaciones
-	glutInit(&argc, argv);							
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);	
-	glutInitWindowSize(500, 500);					
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(50, 50);
 
 	// Crear ventana
-	glutCreateWindow(PROYECTO);		
+	glutCreateWindow(PROYECTO);
 
 	// Registrar callbacks
-	glutDisplayFunc(display);						
-	glutReshapeFunc(reshape);	
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
 
 	// Bucle de atencion a eventos
-	glutMainLoop();									
+	glutMainLoop();
 }
